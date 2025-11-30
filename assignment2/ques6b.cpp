5e42c9d97f47a88bcd653d762eec0147dd26b641
Yashita Aggarwal <yashitaaggarwal.2815@gmail.com>
2025-11-30 14:59:07 +0530

    ques6b

--- assignment2/ques6b.cpp
+++ assignment2/ques6b.cpp
@@ -1,58 +1,59 @@
 #include<iostream>
 using namespace std;
-int main (){
 
+int main (){
     int sp1[4][3]={{3,3,3},{0,1,3},{2,2,6},{1,5,9}};
     int sp2[4][3]={{3,3,3},{3,1,5},{2,2,8},{0,3,7}};
-
     int sp3[30][3];
-    int i=1,j=1,k=1;
+    int i=1, j=1, k=1;
+    int l1 = sp1[0][2];
+    int l2 = sp2[0][2];
 
-    int l1=sp1[0][2];
-    int l2=sp2[0][2];
-    while(i<=l1 && j<=l2){
-        if
-            ( sp1[i][0]<sp2[j][0] || (sp1[i][0]==sp2[j][0] && sp1[i][1]<sp2[j][1]) ){
-                sp3[k][0]=sp1[i][0];
-                sp3[k][1]=sp1[i][1];
-                sp3[k][2]=sp1[k][2];
-                i++;k++;
-        }
-
-        else if   ( sp1[i][0]>sp2[j][0] || (sp1[i][0]==sp2[j][0] && sp1[i][1]>sp2[j][1]) ){
-                sp3[k][0]=sp1[j][0];
-                sp3[k][1]=sp1[j][1];
-                sp3[k][2]=sp1[j][2];
-                j++;k++;
-        }
-        else //same rows and columns value
+    while(i <= l1 && j <= l2){
+        if( sp1[i][0] < sp2[j][0] ||
+           (sp1[i][0] == sp2[j][0] && sp1[i][1] < sp2[j][1]) )
         {
-            sp3[k][0]=sp1[i][0];
-            sp3[k][1]=sp1[i][1];
-            sp3[k][2]=sp1[k][2];
-                j++;i++;k++;
+            sp3[k][0] = sp1[i][0];
+            sp3[k][1] = sp1[i][1];
+            sp3[k][2] = sp1[i][2];
+            i++; k++;
+        } else if( sp1[i][0] > sp2[j][0] ||
+                (sp1[i][0] == sp2[j][0] && sp1[i][1] > sp2[j][1]) )
+        {
+            sp3[k][0] = sp2[j][0];
+            sp3[k][1] = sp2[j][1];
+            sp3[k][2] = sp2[j][2];
+            j++; k++;
+        } else {
+            sp3[k][0] = sp1[i][0];
+            sp3[k][1] = sp1[i][1];
+            sp3[k][2] = sp1[i][2] + sp2[j][2];
+            i++; j++; k++;
         }
     }
 
-    //copy remaining elements
-    while(i<=l1){
-        sp3[k][0]=sp1[i][0];
-                sp3[k][1]=sp1[i][1];
-                sp3[k][2]=sp1[k][2];
-                i++;k++;
-    }
-    while(j<=l2){
-        sp3[k][0]=sp1[j][0];
-                sp3[k][1]=sp1[j][1];
-                sp3[k][2]=sp1[j][2];
-                j++;k++;
+    while(i <= l1){
+        sp3[k][0] = sp1[i][0];
+        sp3[k][1] = sp1[i][1];
+        sp3[k][2] = sp1[i][2];
+        i++; k++;
     }
 
-    sp3[0][2]=k-1;
+    while(j <= l2){
+        sp3[k][0] = sp2[j][0];
+        sp3[k][1] = sp2[j][1];
+        sp3[k][2] = sp2[j][2];
+        j++; k++;
+    }
 
-    cout<<"addition of two sparse matrix:\n";
-    cout<<"rows\tcolumns\tvalues\n";
-    for(int x=0;x<=sp3[0][2];x++){
+    sp3[0][0] = sp1[0][0]; 
+    sp3[0][1] = sp1[0][1];  
+    sp3[0][2] = k - 1;      
+    
+    cout << "Sparse Matrix Addition:\n";
+    cout << "Row\tCol\tVal\n";
+    for(int x=0; x <= sp3[0][2]; x++){
+        cout << sp3[x][0] << "\t" << sp3[x][1] << "\t" << sp3[x][2] << endl;
     }
     return 0;
 }
