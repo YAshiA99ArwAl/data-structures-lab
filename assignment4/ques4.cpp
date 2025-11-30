a11d1a8bf6270979ed2346c240d1dba86b65f4d1
Yashita Aggarwal <yashitaaggarwal.2815@gmail.com>
2025-11-30 15:29:06 +0530

    ques4

--- assignment4/ques4.cpp
+++ assignment4/ques4.cpp
@@ -1,25 +1,22 @@
-#include <iostream>
-#include <queue>
-#include <unordered_map>
+#include<iostream>
+#include<queue>
 using namespace std;
 
 int main() {
-    string s; 
-    cout << "Enter the string: ";
-    getline(cin, s); 
     queue<char> q;
-    unordered_map<char,int> freq;
-    for (char c : s) {
-        if (c == ' ') continue;  
-        freq[c]++;
-        q.push(c);
-        while (!q.empty() && freq[q.front()] > 1) {
+    int freq[26] = {0};
+    char ch;
+    while (cin >> ch) {
+        freq[ch - 'a']++;
+        q.push(ch);
+        while (!q.empty() && freq[q.front() - 'a'] > 1) {
             q.pop();
         }
-        if (!q.empty())
-            cout << q.front() << " ";
-        else
+        if (q.empty())
             cout << -1 << " ";
+        else
+            cout << q.front() << " ";
     }
-    cout << endl;
+
+    return 0;
 }
